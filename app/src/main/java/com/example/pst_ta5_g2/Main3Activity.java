dcfcf8b4bcc52c4f17ce79be51d000fc95838a13
jlchuchu <jlchuchu@espol.edu.ec>
2019-07-22 15:20:13 -0500

    Video de youtube

---
+++ app/src/main/java/com/example/pst_ta5_g2/Main3Activity.java
@@ -0,0 +1,19 @@
+package com.example.pst_ta5_g2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+    public void volver(View view) {
+        finish();
+    }
+}
--- app/src/main/java/com/example/pst_ta5_g2/MainActivity.java
+++ app/src/main/java/com/example/pst_ta5_g2/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.pst_ta5_g2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnUbicacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnUbicacion=(Button) findViewById(R.id.btnUbicacion);
+
+        btnUbicacion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void video(View view) {
+        Intent i = new Intent(this, Main2Activity.class );
+        startActivity(i);
+    }
+
+    public void calendario(View view) {
+        Intent i = new Intent(this, Main3Activity.class );
+        startActivity(i);
     }
 }
